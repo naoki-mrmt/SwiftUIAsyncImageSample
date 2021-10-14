139758cacc5506d38becce0880d76c72c86544f8
Naoki <20cm0135@jec.ac.jp>
2021-10-14 13:44:00 +0900

    Initial Commit

---
+++ SwiftUIAsyncImageSample/SwiftUIAsyncImageSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageSampleApp.swift
+//  SwiftUIAsyncImageSample
+//
+//  Created by cmStudent on 2021/10/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
