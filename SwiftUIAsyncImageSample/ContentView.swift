8e9446f625a8ed4dec7d33a3a6331af7c41cac95
Naoki <20cm0135@jec.ac.jp>
2021-10-14 14:20:32 +0900

    AsyncImageのplaceholder

--- SwiftUIAsyncImageSample/ContentView.swift
+++ SwiftUIAsyncImageSample/ContentView.swift
@@ -16,12 +16,36 @@ struct ContentView: View {
 //        AsyncImage(url: URL(string: imageURL))
         
         // MARK: - 2. SCALE
-        AsyncImage(url: URL(string: imageURL), scale: 3.0)
+//        AsyncImage(url: URL(string: imageURL), scale: 3.0)
+        
+        // MARK: - 3. PLACEHOLDER
+        AsyncImage(url: URL(string: imageURL)) { image in
+            image.imageModifier()
+        } placeholder: {
+            Image(systemName: "photo.circle.fill").iconModifier()
+        }
+        .padding(40)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension Image {
+    func imageModifier() -> some View {
+        self
+            .resizable()
+            .scaledToFit()
+    }
+    
+    func iconModifier() -> some View {
+        self
+            .imageModifier()
+            .frame(maxWidth: 128)
+            .foregroundColor(.purple)
+            .opacity(0.5)
     }
 }
